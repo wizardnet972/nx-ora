1606e2e8ba71f629dc785537ca98b22812e812a8
wizardnet972 <wizardnet972@gmail.com>
2022-11-18 13:49:52 +0200

    init

---
+++ packages/cli/src/lib/cli.ts
@@ -0,0 +1,14 @@
+import * as ora from 'ora';
+
+const spinner1 = ora('Running Plugins...').start();
+
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+console.log('in cli!');
+
+console.log(fibonacci(1990));
+
+spinner1.succeed();
